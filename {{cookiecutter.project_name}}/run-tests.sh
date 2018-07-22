2d8a9e8c8df11a4bf068dbd811f61fc6f5151dbc
Dmitry Voronin <dimka665@gmail.com>
2018-07-23 03:41:28 +0400

    Add tox support

---
+++ {{cookiecutter.project_name}}/run-tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o errexit
+
+pipenv run test
+{% if cookiecutter.docs != 'y' -%}
+pipenv run doctest
+pipenv run build-docs
+{% endif -%}
+
+echo -e "\033[1;32m  Tests passed  \033[1;m"
